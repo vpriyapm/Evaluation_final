4005009975308d3754e10d86ac644cd5d7aa0024
ccsyedbilal <129423306+ccsyedbilal@users.noreply.github.com>
2023-05-06 14:32:01 +0530

    Update dob.java (21BCAF60)
    i have improvements make the code more readable and maintainable, and help to clarify the purpose of the program.

--- dob.java
+++ dob.java
@@ -1,6 +1,11 @@
+/**
+ * This program prints out the user's date of birth.
+ */
+
 public class Main {
   public static void main(String[] args) {
-    String dob = "01/01/2000";
-    System.out.println("My date of birth is " + dob);
+    String dateOfBirth = "01/01/2000"; // Expected format: DD/MM/YYYY
+    String output = String.format("My date of birth is %s", dateOfBirth);
+    System.out.println(output);
   }
 }
