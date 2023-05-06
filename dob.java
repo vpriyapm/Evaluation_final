a1cbfb262885bf35317557c41772c917724ecd6e
joelnencil <joelnencil@gmail.com>
2023-05-06 11:43:24 +0530

    Update dob.java

--- dob.java
+++ dob.java
@@ -1,6 +1,7 @@
 public class Main {
   public static void main(String[] args) {
-    String dob = "01/06/2001";
-    System.out.println("My date of birth is 1st June 2001 " + dob);
+    String dob = "30/06/2002";
+    String name = "Mudassir"
+    System.out.println("My date of birth is 1st June 2001 " + dob "and my name is " +name);
   }
 }
