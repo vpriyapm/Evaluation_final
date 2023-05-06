4ca7a930df6b453b398e3e63b36232401c2df90a
Davis3103 <122588371+Davis3103@users.noreply.github.com>
2023-05-06 09:53:59 +0530

    Update and rename dob.java to changed dob.java
    value of dob is changed

--- changed dob.java
+++ changed dob.java
@@ -1,6 +1,6 @@
 public class Main {
   public static void main(String[] args) {
-    String dob = "01/01/2000";
+    String dob = "01/01/2003";
     System.out.println("My date of birth is " + dob);
   }
 }
