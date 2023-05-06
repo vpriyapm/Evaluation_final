508d2f9d28ffa7caee23e25f1727e8f389f96c6a
STEVIN-JOSEPH-B <122467449+STEVIN-JOSEPH-B@users.noreply.github.com>
2023-05-06 11:47:18 +0530

    21BCAF59

--- dob.java
+++ dob.java
@@ -1,6 +1,6 @@
 public class Main {
   public static void main(String[] args) {
-    String dob = "22/06/2003";
+    String dob = "22/06/2000";
     System.out.println("My date of birth is " + dob);
   }
 }
