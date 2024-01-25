baeed4f221c776a83b75bb7d54880a3cbfb6b352
Diana princy <148611145+Diana1princy@users.noreply.github.com>
2024-01-25 14:23:47 +0530

    Update add.c

--- add.c
+++ add.c
@@ -2,7 +2,7 @@
 
 int main() 
 {
-    int num1, num2, sum;
+    int num1, num2, num3, num4 sum;
     
     printf("Enter the first number: ");
     scanf("%d", &num1);
@@ -10,7 +10,7 @@ int main()
     printf("Enter the second number: ");
     scanf("%d", &num2);
     
-    sum = num1 + num2;
+    sum = num1 + num2 + num3 + num4;
     
     printf("The sum of %d and %d is %d.", num1, num2, sum);
     
