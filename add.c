8a989bfe8e05830f2d95f6a699fb45fdf0b80526
DIYA ANN PHILIP <157470098+diyann1@users.noreply.github.com>
2024-02-06 14:19:07 +0530

    Update add.c

--- add.c
+++ add.c
@@ -10,8 +10,9 @@ int main() {
     scanf("%d", &num2);
     
     sum = num1 + num2;
+    diff=num1-num2;
     
     printf("The sum of %d and %d is %d.", num1, num2, sum);
-    
+    printf("the diff of %d and %d is %d",num1,num2,diff);
     return 0;
 }
