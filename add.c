f15165a8ece786afcfc412dbcc9e685d2ff1f0f7
Isaac Thapa <isaacthapa36@gmail.com>
2024-02-06 14:18:56 +0530

    multiplication

--- add.c
+++ add.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
 
 int main() {
-    int num1, num2, mult;
+    int num1, num2,num3 , mult;
     
     printf("Enter the first number: ");
     scanf("%d", &num1);
     
     printf("Enter the second number: ");
     scanf("%d", &num2);
+
+    printf("Enter the second number: ");
+    scanf("%d", &num2);
+
+    mult= num1 * num 2* num3;
     
-    
-    mult = num1 * num2;
-    
-    printf("The mult of the two numbers %d and %d is %d.", num1, num2, sum);
+    printf("The multiplication of %d and %d and %d is %d.", num1, num2, num3, mult);
     
     return 0;
 }
