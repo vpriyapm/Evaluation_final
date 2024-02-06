db9696975d2f78deab77e4f9746d1fd58abaaef6
AROKIYA RAJ A <155761546+rajproduction@users.noreply.github.com>
2024-02-06 13:56:14 +0530

    Update add.c
    decleared value

--- add.c
+++ add.c
@@ -2,12 +2,13 @@
 
 int main() {
     int num1, num2, sum;
-    
+    num1 = 2;
+    num2 = 2;
     printf("Enter the first number: ");
-    scanf("%d", &num1);
+    
     
     printf("Enter the second number: ");
-    scanf("%d", &num2);
+    
     
     sum = num1 + num2;
     
