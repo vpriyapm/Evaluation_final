90a64b9a253c9272d3d014a40d16b34902e9bd10
HariPrasadReddy B N <156066599+Hariprasadreddy25@users.noreply.github.com>
2024-02-06 14:03:29 +0530

    add.c

--- add.c
+++ add.c
@@ -14,7 +14,7 @@ int main() {
     
     sum = num1 + num2 + num3;
     
-    printf("The sum of %d and %d is %d.", num1, num2, sum);
+    printf("The sum of %d and %d is %d.", num1, num2, num3,sum);
     
     return 0;
 }
