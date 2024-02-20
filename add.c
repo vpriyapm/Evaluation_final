ad89db236ef3c2f7000d05c625a8888c252fb542
MargaretPriyaD <156065730+MargaretPriyaD@users.noreply.github.com>
2024-02-20 14:03:55 +0530

    Update add.c

--- add.c
+++ add.c
@@ -8,10 +8,11 @@ int main() {
     
     printf("Enter the second number: ");
     scanf("%d", &num2);
+    rintf("Enter the third number: ");
+    scanf("%d", &num3);
+    sum = num1 + num2+num3;
     
-    sum = num1 + num2;
-    
-    printf("The sum of %d and %d is %d.", num1, num2, sum);
+    printf("The sum of %d and %d is %d.", num1, num2,num3, sum);
     
     return 0;
 }
