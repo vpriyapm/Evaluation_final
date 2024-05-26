cfc87c9ef937cbb4b5a0a40add139415dfada822
admin <janani.1573.raj@gmail.com>
2024-05-26 14:04:28 +0530

    new prg

--- dob.java
+++ dob.java
@@ -1,6 +1,8 @@
 public class Main {
+
   public static void main(String[] args) {
     String dob = "01/01/2000";
+    string jan = "30/7/2004";
     System.out.println("My date of birth is " + dob);
 string dob = "15/7/2003";
   }
