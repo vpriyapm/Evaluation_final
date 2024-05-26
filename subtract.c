fe35a1a9f4226077a5404e5a61e434383f200dfa
k-vruttika <vruttika13@gmail.com>
2024-05-26 19:23:59 +0530

    commit

---
+++ subtract.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2, sum;
+    
+    printf("Enter the first number: ");
+    scanf("%d", &num1);
+    
+    printf("Enter the second number: ");
+    scanf("%d", &num2);
+    
+    diff = num1 + num2;
+    
+    printf("The diff of %d and %d is %d.", num1, num2, diff);
+    
+    return 0;
+}
