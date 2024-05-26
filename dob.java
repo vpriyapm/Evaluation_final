df43e124ee6c4d0482cd0e4d7c9ef01a770a2b7e
hajirasait003 <156066652+hajirasait003@users.noreply.github.com>
2024-05-26 22:35:35 +0530

    Update dob.java

--- dob.java
+++ dob.java
@@ -1,6 +1,6 @@
 public class Main {
   public static void main(String[] args) {
-    String dob = "31/12/2000";
+    String dob = "31/12/2003";
     System.out.println("My date of birth is " + dob);
   }
 }
