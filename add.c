6da0daabd2dcfe3ecb468cc2fb363faabb2af408
Hariprasadreddy25 <hariprasadreddybn2@gamil.com>
2025-01-02 14:34:02 +0530

    Made the code changes

--- add.c
+++ add.c
@@ -2,18 +2,18 @@
 
 int main() {
 
-    int num1, num2, sum, sub;
-
-    int num, num2,num3, sum,result;
-
+    int num1, num2, num3, sum;
     
     printf("Enter the first number: ");
     scanf("%d", &num1);
     
     printf("Enter the second number: ");
     scanf("%d", &num2);
+
+    printf("Enter the third number: ");
+    scanf("%d", &num3);
     
-    sum = num1 + num2 +num3;
+    sum = num1 + num2 + num3;
     
     printf("The sum of %d and %d is %d.", num1, num2, sum);
     
